003d52b58f3e5c01e36df2cbc2865bddfe55d50f
buba <bubabest91@gmail.com>
2016-04-21 20:42:03 +0300

    Add avatar provider

---
+++ tests/unit/fixtures/providers/Avatar.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link https://github.com/bubasuma/yii2-simplechat
+ * @copyright Copyright (c) 2015 bubasuma
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace bubasuma\simplechat\tests\unit\fixtures\providers;
+
+use Faker\Provider\Base;
+use yii\helpers\FileHelper;
+
+class Avatar extends Base
+{
+    private $_container;
+
+    public function avatar()
+    {
+        $path = __DIR__ . '/../../../../assets/img/avatars';
+        if(null === $this->_container){
+            $this->_container = array_map(
+                function($file){
+                    return basename($file);
+                },
+                FileHelper::findFiles($path)
+            );
+        }
+        return $this->generator->randomElement($this->_container);
+    }
+}
--- tests/unit/fixtures/templates/profile.php
+++ tests/unit/fixtures/templates/profile.php
@@ -4,10 +4,10 @@
  * @var $index integer
  */
 
-$avatarPath =  __DIR__ . '/../../../../assets/img/avatars';
+
 return [
     'first_name' => $faker->firstNameMale,
     'last_name' => $faker->lastName,
     'gender' => 'M',
-    'avatar' => basename($faker->randomElement(\yii\helpers\FileHelper::findFiles($avatarPath))),
+    'avatar' => $faker->avatar,
 ];
